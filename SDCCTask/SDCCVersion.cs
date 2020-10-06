624cf0745b6897f93c0d590dc95b9edf66963952
ssjason123 <ssjason123@gmail.com>
2020-10-05 19:35:43 -0700

    Fix issue where removing all files from a project would cause property pages to fail to load after adding a new file to the project. Add support to expose SDCC defines to visual studio based on the enabled properties. Add SDCCVersion task to extract the version information from the SDCC command line.

---
+++ SDCCTask/SDCCVersion.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace SDCCTask
+{
+    public class SDCCVersion : Task
+    {
+        public string SDCCExecutable { get; set; }
+
+        [Output]
+        public uint MajorVersion { get; set; }
+        [Output]
+        public uint MinorVersion { get; set; }
+        [Output]
+        public uint PatchVersion { get; set; }
+        [Output]
+        public string Version { get; set; }
+        [Output]
+        public uint Revision { get; set; }
+
+
+        /// <inheritdoc />
+        public override bool Execute()
+        {
+            var result = false;
+            // Reset the version info.
+            MajorVersion = 0;
+            MinorVersion = 0;
+            PatchVersion = 0;
+            Version = "0.0.0";
+            Revision = 0;
+
+            // Run the SDCC command and extract the version information.
+            if (File.Exists(SDCCExecutable))
+            {
+                // Execute sdcc.exe -v and regex the version/revision info.
+                var processInfo = new ProcessStartInfo(SDCCExecutable, "-v");
+                processInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                processInfo.RedirectStandardOutput = true;
+                processInfo.UseShellExecute = false;
+                processInfo.CreateNoWindow = true;
+
+                using (var process = Process.Start(processInfo))
+                {
+                    process.WaitForExit();
+
+                    var commandLine = process.StandardOutput.ReadToEnd();
+                    var match = Regex.Match(commandLine, @"([0-9]+)\.([0-9]+)\.([0-9]+) #([0-9]+)");
+
+                    if (match.Success)
+                    {
+                        MajorVersion = uint.Parse(match.Groups[1].Value);
+                        MinorVersion = uint.Parse(match.Groups[2].Value);
+                        PatchVersion = uint.Parse(match.Groups[3].Value);
+                        Revision = uint.Parse(match.Groups[4].Value);
+                        Version = string.Format("{0}.{1}.{2}", MajorVersion, MinorVersion, PatchVersion);
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
