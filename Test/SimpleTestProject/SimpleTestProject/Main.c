cec21ada2446234f5212a402b03a8321d0d19ec8
ssjason123 <ssjason123@gmail.com>
2023-06-21 22:00:59 -0700

    Initial Pass to add SDLD support

--- Test/SimpleTestProject/SimpleTestProject/Main.c
+++ Test/SimpleTestProject/SimpleTestProject/Main.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+
+void test(int arg)
+{
+    //int arg = 890;
+    arg = 123;
+}
+
 void main(void)
 {
-    
+    int a = 123;
+    test(a);
+    //invalid line
+
+    return;
+    a = 234;
 }
---
+++ Test/SimpleTestProject/SimpleTestProject/Test.c
@@ -0,0 +1,2 @@
+int global_value = 999;
+int second_value = 22; 
